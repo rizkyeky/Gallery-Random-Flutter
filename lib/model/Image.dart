9780ab7e7e606f714b9be3b34da41bef53337e28
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-15 21:23:11 +0700

    Update Image.dart
    new image object from api

--- lib/model/Image.dart
+++ lib/model/Image.dart
@@ -14,7 +14,6 @@ class ImageGallery {
     "name": ""
   };
 
-
   @override
   String toString() => "image:$id";
 
@@ -24,6 +23,7 @@ class ImageGallery {
     width = 0;
     height = 0;
     urls = <String, String>{};
+    altDescription = "";
     likes = 0;
   }
 
@@ -36,19 +36,21 @@ class ImageGallery {
     width = double.parse(json["width"].toString());
     height = double.parse(json["height"].toString());
     urls = <String, String>{
-      "full": json["urls"]["full"],
-      "thumb": json["urls"]["thumb"],
-      "html": json["urls"]["html"]
+      "full": json["urls"]["full"].toString(),
+      "thumb": json["urls"]["thumb"].toString(),
+      "html": json["urls"]["html"].toString()
     };
-    // downloadUrl = json["download_url"].toString();
+    altDescription = json["alt_description"].toString();
+    likes = int.parse(json["likes"]);
   }
 
   Map<String, dynamic> toJson(ImageGallery image) => {
-    "id": id,
-    "author": author,
-    "width": width,
-    "height": height,
-    "url": urls,
-    // "download_url": downloadUrl
+    "id": image.id,
+    "user": image.author,
+    "width": image.width,
+    "height": image.height,
+    "urls": image.urls,
+    "likes": image.likes,
+    "alt_description": image.altDescription
   };
 }
