02e7d703c0614dc01ae858241192ba078d5689d7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-16 10:30:39 +0700

    add dio package

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,24 +1,29 @@
-import 'dart:async';
+// import 'dart:async';
 import 'dart:convert';
-import 'dart:io' show InternetAddress, SocketException;
 import 'dart:typed_data';
-import 'package:http/http.dart' as http;
+
+import 'package:dio/dio.dart';
+
 import '../model/Image.dart';
 
 class Api {
-  String url = "https://picsum.photos";
-  final _host = "api.unsplash.com";
 
-  Map<String, String> _header = {
+  final Dio _dio = Dio();
+  final String _host = "api.unsplash.com";
+  final Map<String, dynamic> _header = {
     "Authorization": "Client-ID " + "1DZ0aLedKkebo2tH8RJ2I0psQ2qfmclGZ9YLq7eixDI",
     "Accept-Version": "v1"
   };
-
-  http.Client client = http.Client();
+  
+  Api() {
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options) => options.headers = _header,
+    ));
+  }
 
   Future<void> testGET() async {
     final uri = Uri.https(_host, "photos");
-    final response = await client.get(uri, headers: _header);
+    final response = await _dio.get(uri.path);
     
     print(uri.toString());
     // print(response.body);
@@ -32,11 +37,10 @@ class Api {
 
   Future<Uint8List> getImageGallery(int id, int width, int height) async {
     // final uri = Uri.https(_host, "photos");
-    final response = await client.get("$url/id/$id/$width/$height");
     // final response = await client.get(uri, headers: _header);
     
     // print(response.body);
     
-    return response.bodyBytes;
+    // return response.bodyBytes;
   }
 }
