5950a09045cbb6c6428ef58a5c80b12aa84006f1
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-08-16 18:29:20 +0700

    back to http (dio is fail)

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,46 +1,42 @@
-// import 'dart:async';
+import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
-
-import 'package:dio/dio.dart';
-
+import 'package:http/http.dart' as http;
 import '../model/Image.dart';
 
 class Api {
+  String url = "https://picsum.photos";
+  final _host = "api.unsplash.com";
 
-  final Dio _dio = Dio();
-  final String _host = "api.unsplash.com";
-  final Map<String, dynamic> _header = {
+  Map<String, String> _header = {
     "Authorization": "Client-ID " + "1DZ0aLedKkebo2tH8RJ2I0psQ2qfmclGZ9YLq7eixDI",
     "Accept-Version": "v1"
   };
-  
-  Api() {
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options) => options.headers = _header,
-    ));
-  }
+
+  http.Client client = http.Client();
 
   Future<void> testGET() async {
     final uri = Uri.https(_host, "photos");
-    final response = await _dio.get(uri.path);
+    final response = await client.get(uri, headers: _header);
     
     print(uri.toString());
     // print(response.body);
 
     List data = json.decode(response.body);
-    Map image = data[0] as Map;
+    ImageGallery image = ImageGallery.fromJson(data[0] as Map);
+    
     print(data.length);
-    print(image.keys);
-    print(image);
+    print(image.toString());
+    print(image.urls);
   }
 
   Future<Uint8List> getImageGallery(int id, int width, int height) async {
     // final uri = Uri.https(_host, "photos");
+    final response = await client.get("$url/id/$id/$width/$height");
     // final response = await client.get(uri, headers: _header);
     
     // print(response.body);
     
-    // return response.bodyBytes;
+    return response.bodyBytes;
   }
 }
